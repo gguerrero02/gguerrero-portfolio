c190190f2fe7618fc9848bb663a978886c525979
garrettguerrero <garrettguerrero02@gmail.com>
2024-09-30 01:27:30 -0400

    small steps
    added some text, some interactivity between desktop and mobile view.

--- src/App.js
+++ src/App.js
@@ -14,7 +14,24 @@ function App() {
         >
           Learn React
         </a>
+
+        <a className="App-link">About</a>
+        <a className="App-link">Projects</a>
+        <a className="App-link">Contact</a>
       </header>
+      <div className="App-body">
+        <h1>Garrett Guerrero</h1>
+        <p> I'm a born and raised Texan that recently moved to New York. 
+            At my core, I love to create. Throughout my life I have always had hobbies where
+            I am creating something new out of nothing. Knitting, crochet, woodworking, cooking - 
+            all of these were outlets for me to put pieces of my mind into the zeitgeist, 
+            and programming is no different. 
+            
+            Front-end development gives me the opportunity to mix two beautiful things, design and logic.
+            I strive to create user interfaces that capture the very essence of the program while being
+            intuitive, clean, and pleasing to the eye. Please scroll and take a look at some of the 
+            projects I have helped bring to life </p>
+      </div>
     </div>
   );
 }
